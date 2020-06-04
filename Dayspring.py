7d2e9dfba2238e7c0c3ee7de0168738bf243f923
Dayspring-daiz <66387485+Dayspring-daiz@users.noreply.github.com>
2020-06-04 12:23:25 +0100

    Create Dayspring.py

---
+++ Dayspring.py
@@ -0,0 +1,7 @@
+name = 'Dayspring Eguaosa'
+hng_id = 'HNG-06495'
+prog_lang = 'Python'
+email = 'd.eguaosa@gmail.com'
+
+output = f'Hello World, this is {name} with HNGi7 ID {hng_id} using {prog_lang} for stage 2 task'
+print(output)
